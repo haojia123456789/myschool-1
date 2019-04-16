25a867a50e3648f7a3f4dfee114cece706f5a9df
haojia <2021551911@qq.com>
2019-04-16 15:07:03 +0800

    v2

---
+++ src/main/java/cn/bdqn/quartz/HelloWorld.java
@@ -0,0 +1,14 @@
+package cn.bdqn.quartz;
+
+public class HelloWorld {
+  private String name;
+
+public String getName() {
+	return name;
+}
+
+public void setName(String name) {
+	this.name = name;
+}
+  
+}
